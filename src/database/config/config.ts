9de3a7960259a176d53f4aec80cfb14270b050de
jose-cleiton <cleitons835@gmail.com>
2023-03-11 16:57:27 -0300

    feat: :sparkles: Add dotenv and create config.js file for database connection options

---
+++ src/database/config/config.ts
@@ -0,0 +1,22 @@
+import dotenv from "dotenv";
+import { Options } from "sequelize/types";
+dotenv.config();
+
+
+const environment: string = process.env.NODE_ENV || "test";
+
+
+const options: Options = {
+  host: process.env.DB_HOST || "localhost",
+  port: Number(process.env.DB_PORT) || 3306, 
+  database: `${process.env.DB_NAME || "delivery-app"}`,
+  username: process.env.DB_USER || "root",
+  password: process.env.DB_PASS || "123456",
+  dialect: "mysql",
+  dialectOptions: {
+    timezone: "Z",
+  },
+  logging: false,
+};
+
+export default {options};
